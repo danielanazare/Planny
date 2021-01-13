341e0296a2dd25018b4a8d53101fbae0bf6b87f8
danielanazare <nazare_daniela@yahoo.com>
2021-01-13 05:06:11 +0200

    Status and Priority Form

--- Controllers/StatusController.cs
+++ Controllers/StatusController.cs
@@ -24,6 +24,37 @@ namespace Planny.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+
+            return View("StatusForm");
+        }
+
+        [HttpPost]
+        public ActionResult Save(Status status)
+        {
+            if (status.Id == 0)
+            {
+                _context.Status.Add(status); //just in the memory
+            }
+            else
+            {
+                var statusInDb = _context.Status.Single(p => p.Id == status.Id);
+                statusInDb.Name = status.Name;
+            }
+
+            _context.SaveChanges(); //wraps changes in a transaction
+
+            return RedirectToAction("Index", "Status");
+        }
+        public ActionResult Edit(int id)
+        {
+            var status = _context.Status.SingleOrDefault(p => p.Id == id);
+            if (status == null)
+                return HttpNotFound();
+            return View("StatusForm", status);
+        }
+
         public ActionResult Index()
         {
             var statuses = _context.Status;
